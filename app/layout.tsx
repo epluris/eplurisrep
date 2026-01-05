b0db8ef3ad85178275c68676e419e5d81a21a8dc
eplurisinc <eplurisinc@gmail.com>
2026-01-05 16:28:30 +0000

    modified:   app/layout.tsx 	modified:   package.json 	new file:   src/dataconnect-generated/dataconnect-generated-1.0.0.tgz

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import UserMenu from "./UserMenu";
+import UserMenu from "./components/UserMenu";
 import { 
   FaSearch, 
   FaDatabase, 
